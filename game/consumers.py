f892f07c4f94ae9cbbc028fc960b86ff8e1331d7
Danylo Samedov <danasamedov@gmail.com>
2025-04-05 23:46:26 +0300

    Add phase-based game state and client-side timer and client-side and server-side validation for card selection and hint submition based on game phase

--- game/consumers.py
+++ game/consumers.py
@@ -41,28 +41,34 @@ class GameConsumer(WebsocketConsumer):
 
     def receive(self, text_data=None, bytes_data=None):
         data = json.loads(text_data)
+        print(f"Received message: {data}")  # Debug log
 
         username = self.scope["session"].get("username")
         if not username:
             self.send(text_data=json.dumps({"error": "User not authenticated"}))
             return
         
-        if data["action"] == "card_choice":
+        if data["action"] == "card_choice" and self.current_phase == "round":
             card_id = data.get("card_id")
             card_status = data.get("card_status")
 
             self.card_choice(username, card_id, card_status)
 
-        if data["action"] == "hint_submit":
+        if data["action"] == "hint_submit" and self.current_phase == "hint":
             hint_word = data["hintWord"]
             hint_num = data["hintNum"]
             leader_team = data["leaderTeam"]
 
             add_hint(Game.objects.get(id=self.game_id), leader_team, hint_word, hint_num)
-
             self.hint_receive(hint_word, hint_num)
 
+        if data["action"] == "start_round":
+            print(f"Starting round, current phase: {self.current_phase}")  # Debug log
+            self.current_phase = 'round'
+            self.start_phase_cycle()
+
         if data["action"] == "start_timer":
+            print(f"Timer ended, current phase: {self.current_phase}")  # Debug log
             # Toggle the phase
             if self.current_phase == 'round':
                 self.current_phase = 'hint'
